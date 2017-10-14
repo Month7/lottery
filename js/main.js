330e48f4f69c86768f74fb869e5a58fbbbe050fa
Month7 <31988780+Month7@users.noreply.github.com>
2017-10-14 15:13:59 +0800

    2017/10/14
    全面更新的主页

--- js/main.js
+++ js/main.js
@@ -277,14 +277,11 @@ $("img.big").load(function(){
     })
 });
 
+/*=================================鼠标滑过============================*/
 
-    $('.log-btn').click(function(){
-        $('.log-status').addClass('wrong-entry');
-       $('.alert').fadeIn(500);
-       setTimeout( "$('.alert').fadeOut(1500);",3000 );
-    });
-    $('.form-control').keypress(function(){
-        $('.log-status').removeClass('wrong-entry');
-    });
 
 });
+
+
+
+

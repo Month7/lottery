f2440202ad33740945f15e780638c77b8fe3a737
Month7 <31988780+Month7@users.noreply.github.com>
2017-10-18 21:04:36 +0800

    2017/10/18
    1在一定程度上解决了不同分辨率的兼容问题
    2增加了发布产品的页面

--- js/main.js
+++ js/main.js
@@ -277,11 +277,26 @@ $("img.big").load(function(){
     })
 });
 
-/*=================================鼠标滑过============================*/
-
-
+/*=================================浏览器兼容============================*/
 });
-
-
+function adjustStyle(width) { 
+    width = parseInt(width); 
+    if ((width < 1700)&&(width>=1400)) { 
+        $("#size-stylesheet").attr("href", "css/浏览器兼容/md.css"); 
+    } else if ((width >= 1701) && (width < 2000)) { 
+        $("#size-stylesheet").attr("href", "css/浏览器兼容/lg.css"); 
+    } else if((width>=1000)&&(width<1400)){
+        $("#size-stylesheet").attr("href", "css/浏览器兼容/sm.css"); 
+    } 
+    else { 
+       $("#css").attr("href", "<?php bloginfo('stylesheet_url'); ?>"); 
+    } 
+} 
+$(function() { 
+    adjustStyle($(this).width()); 
+    $(window).resize(function() { 
+        adjustStyle($(this).width()); 
+    }); 
+});
 
 

43ba6d6bce3330ae592b9f5de51704863bbbb3e3
Month7 <31988780+Month7@users.noreply.github.com>
2017-11-01 13:36:41 +0800

    2017/11/1
    进行了前端性能优化：
    1、合并了css文件，减少了http请求
    2、抛弃了fonts.useso字体库
    3、jQuery代码优化

--- js/miaosha.js
+++ js/miaosha.js
@@ -1,12 +1,13 @@
 /*=================================浏览器兼容============================*/
 function adjustStyle(width) { 
     width = parseInt(width); 
+    $style=$("#size-stylesheet");
     if ((width < 1700)&&(width>=1400)) { 
-        $("#size-stylesheet").attr("href", "css/浏览器兼容/秒杀/md.css"); 
+        $style.attr("href", "css/浏览器兼容/秒杀/md.css"); 
     } else if ((width >= 1701) && (width < 2000)) { 
-        $("#size-stylesheet").attr("href", "css/浏览器兼容/秒杀/lg.css"); 
+        $style.attr("href", "css/浏览器兼容/秒杀/lg.css"); 
     } else if((width>=1000)&&(width<1400)){
-        $("#size-stylesheet").attr("href", "css/浏览器兼容/秒杀/sm.css"); 
+        $style.attr("href", "css/浏览器兼容/秒杀/sm.css"); 
     } 
     else { 
        $("#css").attr("href", "<?php bloginfo('stylesheet_url'); ?>"); 
